5868c4a27dbd06454486971476bbc294397d8471
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-18 18:32:08 +0900

    Initial commit

---
+++ src/components/PostSkeleton.jsx
@@ -0,0 +1,66 @@
+const PostSkeleton = () => {
+  return (
+    <div className='single-post-content fill-screen single-post-content-skeleton'>
+      <div className='skeleton skeleton-width-100 skeleton-title'></div>
+      <div className='skeleton skeleton-width-80 skeleton-title margin-bottom-1'></div>
+      <div className='skeleton skeleton-width-20 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text margin-bottom-2'></div>
+
+      <div className='skeleton skeleton-width-50 skeleton-text margin-top-2'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-50 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-50 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text'></div>
+      <div className='skeleton skeleton-width-50 skeleton-text margin-bottom-1'></div>
+      <div className='skeleton skeleton-width-20 skeleton-text margin-top-1 margin-bottom-3'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-3'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text margin-bottom-1'></div>
+
+      <div className='skeleton skeleton-width-100 skeleton-text margin-top-1'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+      <div className='skeleton skeleton-width-100 skeleton-text'></div>
+    </div>
+  );
+};
+
+export default PostSkeleton;
