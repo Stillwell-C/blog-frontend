bd6bb03281a69fbec97e85cca4ab50d195fc9460
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-20 10:22:07 +0900

    Add skip to begin/end & accessibility

--- src/components/AllPostsPageBtns.jsx
+++ src/components/AllPostsPageBtns.jsx
@@ -51,22 +51,50 @@ const AllPostsPageBtns = ({ totalPages, currentPage, setCurrentPage }) => {
   return (
     <nav
       className='post-pagination-nav'
-      aria-label='All Posts Pagination Navigation'
+      aria-label='All Posts Page Pagination Navigation'
     >
       <button
         type='button'
         className='posts-pagination-btn posts-pagination-btn-small basic-button'
         disabled={currentPage <= 1 ? true : false}
-        onClick={() => setCurrentPage((prev) => prev - 1)}
+        aria-disabled={currentPage <= 1 ? true : false}
+        aria-label='Skip to page 1.'
+        onClick={() => setCurrentPage(1)}
       >
         &laquo;
+      </button>
+      <button
+        type='button'
+        className='posts-pagination-btn posts-pagination-btn-small basic-button'
+        disabled={currentPage <= 1 ? true : false}
+        aria-disabled={currentPage <= 1 ? true : false}
+        aria-label={`Move one page back ${
+          currentPage !== 1 ? `to page ${currentPage - 1}` : ""
+        }`}
+        onClick={() => setCurrentPage((prev) => prev - 1)}
+      >
+        &lsaquo;
       </button>
       {content}
       <button
         type='button'
         className='posts-pagination-btn posts-pagination-btn-small basic-button'
         disabled={currentPage === totalPages ? true : false}
+        aria-disabled={currentPage === totalPages ? true : false}
+        aria-label={`Move one page forward ${
+          currentPage !== totalPages ? `to page ${currentPage + 1}` : ""
+        }`}
         onClick={() => setCurrentPage((prev) => prev + 1)}
+      >
+        &rsaquo;
+      </button>
+      <button
+        type='button'
+        className='posts-pagination-btn posts-pagination-btn-small basic-button'
+        disabled={currentPage === totalPages ? true : false}
+        aria-disabled={currentPage === totalPages ? true : false}
+        aria-label={`Skip to last page, page ${totalPages}`}
+        onClick={() => setCurrentPage(totalPages)}
       >
         &raquo;
       </button>
