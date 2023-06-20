7651056b7ad6975d795aaae7e6576dd833cc6743
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-20 11:44:08 +0900

    Add comments section/like button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import axios from "../api/axios";
 import PostSkeleton from "./PostSkeleton";
 import dateOptions from "../utils/DateOptions";
+import outlinedHeart from "../assets/heart-outline.svg";
+import filledHeart from "../assets/heart-filled.svg";
 
 const POST_URL = "/posts";
 
@@ -44,25 +46,39 @@ const Post = () => {
     axiosGet();
   }, []);
 
-  //   const LoadingPage = <p>Loading...</p>;
-
   const LoadedPage = (
-    <article className='single-post-content'>
-      <h2>{postContent?.title}</h2>
-      <div className='single-post-author'>
-        <span>By: {postContent?.author?.username}</span>
+    <>
+      <div className='single-post-content'>
+        <article>
+          <h2>{postContent?.title}</h2>
+          <div className='single-post-author'>
+            <span>By: {postContent?.author?.username}</span>
+          </div>
+          <div className='single-post-date'>
+            <span>{parsedDate}</span>
+          </div>
+          <div className='single-post-epigraph-content'>
+            <p className='epigraph-text'>{postContent?.epigraph}</p>
+            <p className='epigraph-author'>{`  - ${postContent?.epigraphAuthor}`}</p>
+          </div>
+          <div className='single-post-text'>
+            <p>{postContent?.text}</p>
+          </div>
+        </article>
+        <div className='post-like-btn-div'>
+          <button
+            type='button'
+            className='post-like-btn basic-button'
+            aria-label={`Like this post. Current likes: ${postContent?.likes}`}
+          >
+            <img src={outlinedHeart} alt='' /> {postContent?.likes}
+          </button>
+        </div>
+        <div className='post-page-break'></div>
+        <h3>Comments</h3>
+        <div className='post-user-comments'></div>
       </div>
-      <div className='single-post-date'>
-        <span>{parsedDate}</span>
-      </div>
-      <div className='single-post-epigraph-content'>
-        <p className='epigraph-text'>{postContent?.epigraph}</p>
-        <p className='epigraph-author'>{`  - ${postContent?.epigraphAuthor}`}</p>
-      </div>
-      <div className='single-post-text'>
-        <p>{postContent?.text}</p>
-      </div>
-    </article>
+    </>
   );
 
   const content = isLoading ? <PostSkeleton /> : LoadedPage;
