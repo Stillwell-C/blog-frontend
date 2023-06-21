ee0b695406f5031814f940219129ee69c5bbb951
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-21 13:26:06 +0900

    Add rtk queries

--- src/components/AllPosts.jsx
+++ src/components/AllPosts.jsx
@@ -1,41 +1,30 @@
 import { useEffect, useState } from "react";
-import axios from "../app/api/axios";
 import PostDisplayLarge from "./PostDisplayLarge";
 import PostDisplayLargeSkeleton from "./PostDisplayLargeSkeleton";
 import AllPostsPageBtns from "./AllPostsPageBtns";
-
-const POST_URL = "/posts";
+import { useGetMultiplePostsQuery } from "../features/posts/postsApiSlice";
 
 const AllPosts = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPosts, setTotalPosts] = useState();
   const [totalPages, setTotalPages] = useState();
+
+  const { data: postData, isFetching } = useGetMultiplePostsQuery({
+    page: currentPage,
+    limit: 10,
+    top: false,
+  });
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [currentPage]);
 
   useEffect(() => {
-    const axiosGet = async () => {
-      setIsLoading(true);
-      try {
-        const response = await axios.get(
-          `${POST_URL}?page=${currentPage}&limit=10`
-        );
-        setTotalPosts(response?.data?.totalPosts);
-        setTotalPages(Math.ceil(response?.data?.totalPosts / 10) || 1);
-        setPosts(response?.data?.posts);
-        setIsLoading(false);
-        console.log(response.data);
-      } catch (err) {
-        console.log(err);
-        //maybe navigate to 404
-      }
-    };
-    axiosGet();
-  }, [currentPage]);
+    // setTotalPosts(postData?.totalPosts);
+    setTotalPages(Math.ceil(postData?.totalPosts / 10) || 1);
+    setPosts(postData?.posts);
+  }, [isFetching]);
 
   let skeletonPosts = [];
   for (let i = 0; i < 10; i++) {
@@ -45,10 +34,11 @@ const AllPosts = () => {
   return (
     <section className='all-posts-container fill-screen'>
       <h2>All Posts</h2>
-      {isLoading && skeletonPosts}
-      {!isLoading &&
+      {isFetching && skeletonPosts}
+      {!isFetching &&
+        posts &&
         posts.map((post) => <PostDisplayLarge post={post} key={post._id} />)}
-      {!isLoading && (
+      {!isFetching && posts && (
         <AllPostsPageBtns
           totalPages={totalPages}
           currentPage={currentPage}
