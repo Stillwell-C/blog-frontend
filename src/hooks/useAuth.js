70c9b06b95514daabc0711fabfb7ce04be3f0d75
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-26 17:40:55 +0900

    Add id to object returned

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -9,14 +9,21 @@ const useAuth = () => {
 
   if (token) {
     const decodedToken = jwtDecode(token);
-    const { username, roles } = decodedToken.UserInfo;
+    const { username, roles, id } = decodedToken.UserInfo;
 
     isContributor = roles.some((role) => role.match(/contributor/i));
     isAdmin = roles.some((role) => role.match(/admin/i));
 
-    return { username, roles, isAdmin, isContributor, loggedIn: true };
+    return { username, id, roles, isAdmin, isContributor, loggedIn: true };
   } else {
-    return { username: "", roles: [], isAdmin, isContributor, loggedIn: false };
+    return {
+      username: "",
+      id: "",
+      roles: [],
+      isAdmin,
+      isContributor,
+      loggedIn: false,
+    };
   }
 };
 
