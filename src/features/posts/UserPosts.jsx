434903403f123afd9aab844e02c6ac0214e9ae39
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-26 19:33:13 +0900

    Initial commit

---
+++ src/features/posts/UserPosts.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { useGetUserPostsQuery } from "../users/usersApiSlice";
+import PostDisplayLargeAbbr from "../../components/PostDisplayLargeAbbr";
+import PostDisplayLargeAbbrSkeleton from "../../components/PostDisplayLargeAbbrSkeleton";
+import PaginationButtons from "../../components/PaginationButtons";
+import ErrorPage from "../../components/ErrorPage";
+import { useSearchParams } from "react-router-dom";
+
+const UserPosts = () => {
+  const { id: userId } = useAuth();
+  const [searchParams, setSearchParams] = useSearchParams(1);
+
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+
+  const {
+    data: postData,
+    isFetching,
+    isError,
+  } = useGetUserPostsQuery({ userId, page: currentPage, limit: 10 });
+
+  useEffect(() => {
+    // setTotalPosts(postData?.totalPosts);
+    setTotalPages(Math.ceil(postData?.totalPosts / 10) || 1);
+    setPosts(postData?.posts);
+  }, [postData]);
+
+  let skeletonPosts = [];
+  for (let i = 0; i < 10; i++) {
+    skeletonPosts.push(<PostDisplayLargeAbbrSkeleton key={i} />);
+  }
+
+  useEffect(() => console.log(postData), [postData]);
+
+  return (
+    <div>
+      {isFetching && skeletonPosts}
+      {!isFetching &&
+        posts &&
+        posts.map((post) => (
+          <PostDisplayLargeAbbr post={post} key={post._id} />
+        ))}
+      {!isFetching && posts && (
+        <PaginationButtons
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      )}
+      {isError && <ErrorPage />}
+    </div>
+  );
+};
+
+export default UserPosts;
