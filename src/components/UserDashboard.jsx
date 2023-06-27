d5146537ece0ffee2289f51abb27dff061a162e9
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-27 12:50:41 +0900

    Add user posts

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,14 +1,42 @@
+import { Link } from "react-router-dom";
 import UserPosts from "../features/posts/UserPosts";
 import useAuth from "../hooks/useAuth";
+import { useState } from "react";
 
 const UserDashboard = () => {
   const { username, roles } = useAuth();
+  const [displayContent, setDisplayContent] = useState("posts");
+
+  let content;
+  if (displayContent === "posts") {
+    content = <UserPosts />;
+  }
 
   return (
-    <section className='fill-screen flex-container flex-align-center flex-justify-center flex-column'>
-      <div>Current User: {username}</div>
-      <div>Account Status: {roles.join(", ")}</div>
-      <UserPosts />
+    <section className='fill-screen flex-container flex-justify-center'>
+      <div className='user-dashboard-content margin-top-2'>
+        <h2>Username: {username}</h2>
+        <p>Account Status: {roles.join(", ")}</p>
+        <div className='user-dashboard-display-buttons margin-top-1'>
+          <button
+            type='button'
+            aria-label='Display your posts'
+            style={displayContent === "posts" ? { fontWeight: "bold" } : {}}
+            onClick={() => setDisplayContent("posts")}
+          >
+            Posts
+          </button>
+          <button
+            type='button'
+            aria-label='Display your comments'
+            style={displayContent === "comments" ? { fontWeight: "bold" } : {}}
+            onClick={() => setDisplayContent("comments")}
+          >
+            Comments
+          </button>
+        </div>
+        {content}
+      </div>
     </section>
   );
 };
