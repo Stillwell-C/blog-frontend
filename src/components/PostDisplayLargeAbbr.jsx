0124a0b131a2174e4f993ae92cae3658ed1f4995
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-30 11:36:03 +0900

    Add admin delete capabilities

--- src/components/PostDisplayLargeAbbr.jsx
+++ src/components/PostDisplayLargeAbbr.jsx
@@ -1,9 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import dateOptions from "../utils/DateOptions";
+import { BeatLoader } from "react-spinners";
+import { useDeletePostMutation } from "../features/posts/postsApiSlice";
+import useAuth from "../hooks/useAuth";
 
-const PostDisplayLargeAbbr = ({ post }) => {
+const PostDisplayLargeAbbr = ({ post, adminDisplay = false }) => {
   const [date, setDate] = useState("");
+  const errRef = useRef();
+
+  const { isAdmin } = useAuth();
+
+  const [deletePost, { isLoading, isSuccess, isError, error }] =
+    useDeletePostMutation();
 
   const parseDate = (createdDate, updatedDate) => {
     if (createdDate === updatedDate) {
@@ -22,6 +31,46 @@ const PostDisplayLargeAbbr = ({ post }) => {
     parseDate(post.createdAt, post.updatedAt);
   }, []);
 
+  const buttonContent = !isLoading ? (
+    "Delete"
+  ) : (
+    <BeatLoader color='#CC0000' size={8} />
+  );
+
+  const handleDelete = async () => {
+    if (!isAdmin) return;
+    await deletePost({ id: post._id });
+  };
+
+  useEffect(() => {
+    if (isError && adminDisplay) errRef.current.focus();
+  }, [isError]);
+
+  let adminButtons = null;
+  if (adminDisplay) {
+    adminButtons = (
+      <>
+        <div className='flex-container flex-align-center flex-justify-center margin-top-1'>
+          <button
+            type='button'
+            className='basic-button delete-button flex-container flex-justify-center flex-align-center'
+            onClick={() => handleDelete({ id: post._id })}
+            disabled={isLoading ? true : false}
+          >
+            {buttonContent}
+          </button>
+        </div>
+        <div
+          className={isError ? "form-error-div" : "error-offscreen"}
+          ref={errRef}
+          aria-live='assertive'
+        >
+          {error}
+        </div>
+      </>
+    );
+  }
+
   return (
     <article className='post-display-wrapper post-display-abbr-wrapper'>
       <div>
@@ -30,6 +79,7 @@ const PostDisplayLargeAbbr = ({ post }) => {
         </Link>
       </div>
       <p className='pdl-date'>{date}</p>
+      {adminButtons}
     </article>
   );
 };
