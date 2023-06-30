0eb9df71f72dbbbab1bacbda1524dd2d3e92cf2c
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-06-30 18:50:29 +0900

    Add edit user info button

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -2,9 +2,13 @@ import UserPosts from "../features/posts/UserPosts";
 import useAuth from "../hooks/useAuth";
 import { useState } from "react";
 import UserComments from "../features/comments/UserComments";
+import { useNavigate } from "react-router-dom";
 
 const UserDashboard = () => {
   const { username, roles } = useAuth();
+
+  const navigate = useNavigate();
+
   const [displayContent, setDisplayContent] = useState("posts");
 
   let content;
@@ -17,7 +21,15 @@ const UserDashboard = () => {
   return (
     <section className='fill-screen flex-container flex-justify-center'>
       <div className='dashboard-content margin-top-2'>
-        <h2 className='margin-btm-1'>My Page</h2>
+        <div className='flex-container flex-align-center margin-btm-1'>
+          <h2 className='margin-r-2'>My Page</h2>
+          <button
+            className='basic-button'
+            onClick={() => navigate("/mypage/edit")}
+          >
+            Edit user info
+          </button>
+        </div>
         <p>Username: {username}</p>
         <p>Account Status: {roles.join(", ")}</p>
         <div className='dashboard-display-buttons margin-top-1'>
