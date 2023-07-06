bbc0f49c4e872d354bbc9d1cc4df32e92da293ef
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-06 14:45:50 +0900

    Add confirm modal for delete

--- src/components/CommentDisplayAbbr.jsx
+++ src/components/CommentDisplayAbbr.jsx
@@ -3,10 +3,16 @@ import dateOptions from "../utils/DateOptions";
 import { useNavigate } from "react-router-dom";
 import { useDeleteCommentMutation } from "../features/comments/commentsApiSlice";
 import { BeatLoader } from "react-spinners";
+import ConfirmModal from "./ConfirmModal";
 
 const CommentDisplayAbbr = ({ comment }) => {
   const navigate = useNavigate();
   const [date, setDate] = useState("");
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [confirmDelete, setConfirmDelete] = useState(false);
+
+  const modalText = "delete this comment";
 
   const parseDate = (createdDate, updatedDate) => {
     if (createdDate === updatedDate) {
@@ -43,6 +49,16 @@ const CommentDisplayAbbr = ({ comment }) => {
     <BeatLoader color='#CC0000' size={8} />
   );
 
+  const handleDelete = async () => {
+    await deleteComment({ id: comment._id });
+  };
+
+  useEffect(() => {
+    if (confirmDelete) {
+      handleDelete();
+    }
+  }, [confirmDelete]);
+
   return (
     <div className='comment-display' role='comment'>
       <p>{commentDisplay}</p>
@@ -58,12 +74,20 @@ const CommentDisplayAbbr = ({ comment }) => {
         <button
           type='button'
           className='basic-button delete-button flex-container flex-justify-center flex-align-center'
-          onClick={() => deleteComment({ id: comment._id })}
+          onClick={() => setModalOpen(true)}
           disabled={isLoading ? true : false}
         >
           {buttonContent}
         </button>
       </div>
+      {modalOpen && (
+        <ConfirmModal
+          text={modalText}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+          setConfirmTask={setConfirmDelete}
+        />
+      )}
     </div>
   );
 };
