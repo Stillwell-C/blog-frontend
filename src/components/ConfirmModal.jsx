5d19b06b557ae154e176ae605cfe011c4504b3f7
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-13 18:03:07 +0900

    Add admin delete password

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import FocusTrap from "focus-trap-react";
 
-const ConfirmModal = ({ text, modalOpen, setModalOpen, setConfirmTask }) => {
+const ConfirmModal = ({
+  text,
+  modalOpen,
+  setModalOpen,
+  setConfirmTask,
+  setAdminPassword,
+  adminConfirm = false,
+}) => {
   useEffect(() => {
     //This will close modal with escape key
     function keyListener(e) {
@@ -15,7 +22,26 @@ const ConfirmModal = ({ text, modalOpen, setModalOpen, setConfirmTask }) => {
     return () => document.removeEventListener("keydown", keyListener);
   });
 
+  const errRef = useRef();
+
+  const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
+
+  useEffect(() => {
+    setErrorText("");
+  }, [password]);
+
   const handleConfirmTask = () => {
+    if (adminConfirm) {
+      if (!password) {
+        setErrorText("You must enter a password to proceed.");
+        errRef?.current?.focus();
+        return;
+      }
+      if (password) {
+        setAdminPassword(password);
+      }
+    }
     setConfirmTask(true);
     setModalOpen(false);
   };
@@ -41,6 +67,23 @@ const ConfirmModal = ({ text, modalOpen, setModalOpen, setConfirmTask }) => {
           </header>
           <section className='modal-body fill-screen flex-container flex-column flex-align-center flex-justify-center'>
             <p className='margin-btm-2'>Are you sure you want to {text}?</p>
+            {adminConfirm && (
+              <div className='admin-confirm-container margin-btm-1'>
+                <p className='margin-btm-1'>
+                  To complete the following action, please enter the
+                  administrator password.
+                </p>
+                <input
+                  type='password'
+                  className='margin-btm-1'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className='admin-confirm-error err-text' ref={errRef}>
+                  {errorText}
+                </div>
+              </div>
+            )}
             <div className='modal-buttons flex-container flex-align-center flex-justify-center'>
               <button
                 type='button'
