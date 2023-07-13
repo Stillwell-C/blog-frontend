de3564be0ca7b46b8863889aa09eb0bdde8ca21d
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-13 12:09:56 +0900

    Update error message

--- src/features/posts/AdminPosts.jsx
+++ src/features/posts/AdminPosts.jsx
@@ -1,16 +1,15 @@
-import { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth";
+import { useEffect, useRef, useState } from "react";
 import PaginationButtons from "../../components/PaginationButtons";
 import { useGetMultiplePostsQuery } from "./postsApiSlice";
 import PostDisplayLargeAbbrSkeleton from "../../components/PostDisplayLargeAbbrSkeleton";
 import PostDisplayLargeAbbr from "../../components/PostDisplayLargeAbbr";
 
 const AdminPosts = () => {
-  const { isAdmin } = useAuth();
-
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState();
+
+  const errRef = useRef();
 
   const {
     data: postData,
@@ -28,6 +27,10 @@ const AdminPosts = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [currentPage]);
+
+  useEffect(() => {
+    if (isError) errRef?.current?.focus();
+  }, [isError]);
 
   let skeletonPosts = [];
   for (let i = 0; i < 10; i++) {
@@ -57,17 +60,17 @@ const AdminPosts = () => {
   let errorContent;
   if (isError) {
     errorContent = (
-      <p className='margin-top-1'>
+      <p className='margin-top-1 .err-text' ref={errRef}>
         An error occurred. Please refresh the page.
       </p>
     );
   }
   return (
     <section className='flex-container flex-column flex-align-center'>
+      {errorContent}
       {isFetching && skeletonPosts}
       {postContent}
       {buttonContent}
-      {errorContent}
     </section>
   );
 };
