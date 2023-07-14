c1b685f263be32304d71a825a8dbb0cd5bc320f0
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-14 15:39:59 +0900

    Condense css for error pages

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = ({ message }) => {
     content = "Please try again";
   }
   return (
-    <section className='fill-screen error-page'>
+    <section className='fill-screen error-page flex-container flex-column flex-align-center flex-justify-center'>
       <img src={errorRobot} alt='' />
       <h2>Error</h2>
       <p>Something went wrong</p>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import birdImg from "../assets/frog-in-mouth.svg";
 
 const NotFound = () => {
   return (
-    <section className='error-page fill-screen'>
+    <section className='error-page fill-screen flex-container flex-column flex-align-center flex-justify-center'>
       <img src={birdImg} alt='' />
       <h2>404 Not Found</h2>
       <p>seems you&apos;re on your own wild goose chase</p>
