38b0b5e51d72407793d061c83ebf15fc15470e5e
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-18 09:54:32 +0900

    Remove unused code and console logs

--- src/features/posts/AllPosts.jsx
+++ src/features/posts/AllPosts.jsx
@@ -6,7 +6,6 @@ import { useGetMultiplePostsQuery } from "./postsApiSlice";
 import ErrorPage from "../../components/ErrorPage";
 
 const AllPosts = () => {
-  // const [isLoading, setIsLoading] = useState(true);
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState();
@@ -22,27 +21,14 @@ const AllPosts = () => {
     top: false,
   });
 
-  // useEffect(() => {
-  //   console.log("Page: ", currentPage);
-  // }, [currentPage]);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [currentPage]);
 
   useEffect(() => {
-    // setTotalPosts(postData?.totalPosts);
     setTotalPages(Math.ceil(postData?.totalPosts / 10) || 1);
     setPosts(postData?.posts);
-    console.log("page: ", currentPage, "data: ", postData);
   }, [postData]);
-
-  // useEffect(() => {
-  //   // setTotalPosts(postData?.totalPosts);
-  //   setTotalPages(Math.ceil(postData?.totalPosts / 10) || 1);
-  //   setPosts(postData?.ids.map((id) => postData.entities[id]));
-  //   console.log("page: ", currentPage, "data: ", postData);
-  // }, [postData]);
 
   let skeletonPosts = [];
   for (let i = 0; i < 10; i++) {
