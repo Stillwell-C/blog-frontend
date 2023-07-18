3a8ff4875ba459e1902c4906660329672e8a4549
Christopher Stillwell <stillwell.c.m@gmail.com>
2023-07-18 13:23:28 +0900

    Disallow react devtools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
